af9ea04e4247a8768c6e82450d27e660d72b0c6d
Austin Johnson <Technicallyaustin@gmail.com>
2022-09-14 23:13:52 -0700

    added comments for future progress, updated Miro Board with intended features

---
+++ flip-that-frontend/scripts/api.js
@@ -0,0 +1,24 @@
+const BACKEND_URL = 'http://localhost:3000';
+const PLAYLIST_URL = '${BACKEND_URL}/playlists';
+const APPLE_TEST_PLAYLIST = 'https://music.apple.com/us/playlist/alt-ctrl/pl.0b593f1142b84a50a2c1e7088b3fb683'
+
+
+
+// Review Rails API and JS workflow.
+
+// API Calls
+
+// fetch the playlist web page
+
+// Identifiy the attribute tags
+
+// Decide how you will display playlists
+
+// Create a Nav Bar on Index.html
+
+// Decide between scraping the page with JS or Nokogiri
+
+// Add a Text bot for URL input
+
+// Add a submit button that post that input to rails
+
