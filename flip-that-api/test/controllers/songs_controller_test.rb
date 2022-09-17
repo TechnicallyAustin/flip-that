dfdf47057759c52679559793c93537a293761f38
Austin Johnson <Technicallyaustin@gmail.com>
2022-09-16 21:13:57 -0700

    added more tests

--- flip-that-api/test/controllers/songs_controller_test.rb
+++ flip-that-api/test/controllers/songs_controller_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class SongsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  
+def test_has_index_method
+end
+
+def test_has_show_method
+end
+
+def test_has_new_method
+end
+
+def test_has_create_method
+end
+
+def test_has_edit_method
+end
+
+def test_has_update_method
+end
+
+def test_has_delete_method
+end
+
 end
